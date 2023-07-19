ef218904bf30a96e4e57d3e269335b4fb87840fe
mrtuxa <mrtuxa@fedora.(none)>
2023-07-19 05:18:53 +0200

    added some part of stops/reachable-from

--- src/include/transport.hpp
+++ src/include/transport.hpp
@@ -7,4 +7,9 @@ struct db {
     static std::string stations() {
         return baseUrl() + "stations/";
     }
+    class stops {
+        static std::string reachable_from(int latitude, int longtitude, std::string address) {
+            return baseUrl() + "stops/reachable-from?latitude=" + std::to_string(latitude) + "&longtitude=" + std::to_string(longtitude) + "&address=" + address;
+        }
+    };
 };
