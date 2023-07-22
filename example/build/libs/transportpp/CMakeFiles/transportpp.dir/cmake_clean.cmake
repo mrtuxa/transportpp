2c910b1a53f3dee31a79033835e9f7c33b87223e
mrtuxa <mrtuxa@fedora.(none)>
2023-07-22 13:48:12 +0200

    changes

---
+++ example/build/libs/transportpp/CMakeFiles/transportpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transportpp.dir/src/library.cpp.o"
+  "CMakeFiles/transportpp.dir/src/library.cpp.o.d"
+  "libtransportpp.a"
+  "libtransportpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transportpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ example/build/libs/transportpp/CMakeFiles/transportpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for transportpp.
+# This may be replaced when dependencies are built.
